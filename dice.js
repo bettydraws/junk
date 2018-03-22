10c2e53e80e5da8eec46018c11e8aca53e818d09
bettydraws <31779007+bettydraws@users.noreply.github.com>
2018-03-21 22:47:50 -0400

    Added Composite view, each side has a dice

--- dice.js
+++ dice.js
@@ -4,7 +4,37 @@ const { ui, ImageView} = require('tabris');
 const IMAGE_PATH = 'https://mrmccormack.github.io/imd-learning-tabris/images/';
 const DICE_OFFSET = 30;
 
+let diceImage1 = new Imagconst {Composite, ui, ImageView} = require('tabris');
+// example of images side by side - Matt.
+
+const IMAGE_PATH = 'https://mrmccormack.github.io/imd-learning-tabris/images/';
+const DICE_OFFSET = 30;
+
+let composite1 = new Composite({
+  left: 0, top: 0, bottom: 0, right: '50%',
+  background: '#f3f3f3'
+}).appendTo(ui.contentView);
+
 let diceImage1 = new ImageView({
+  centerY: 0,
+  centerX: -DICE_OFFSET,
+  image: IMAGE_PATH + '6.png'
+}).appendTo(ui.contentView);
+
+let composite2 = new Composite({
+  left: '50%', top: 0, bottom: 0, right: 0,
+  background: '#eaeaea'
+}).appendTo(ui.contentView);
+
+
+let diceImage2 = new ImageView({
+  centerY: 0,
+  centerX: DICE_OFFSET,
+  image: IMAGE_PATH + '1.png'
+
+}).appendTo(ui.contentView);
+
+eView({
   centerY: 0,
   centerX: -DICE_OFFSET,
   image: IMAGE_PATH + '6.png'
