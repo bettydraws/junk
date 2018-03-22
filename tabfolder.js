6d6568e983e5cf3ee2c54668f66d9596e1a2990c
bettydraws <31779007+bettydraws@users.noreply.github.com>
2018-03-21 22:39:43 -0400

    Trying to add composite view to one of the three sections (split screen)

--- tabfolder.js
+++ tabfolder.js
@@ -1,4 +1,4 @@
-const {Tab, TabFolder, TextView, ui} = require('tabris');
+const {Composite, Tab, TabFolder, TextView, ui} = require('tabris');
 
 const IMAGE_PATH = 'https://raw.githubusercontent.com/eclipsesource/tabris-js/master/snippets/resources/';
 
@@ -8,6 +8,28 @@ let tabFolder = new TabFolder({
   left: 0, top: 0, right: 0, bottom: 0,
   paging: true // enables swiping. To still be able to open the developer console in iOS, swipe from the bottom right.
 }).appendTo(ui.contentView);
+
+let composite1 = new Composite({
+  left: 0, top: 0, bottom: 0, right: '50%',
+  background: '#f3f3f3'
+}).appendTo(ui.contentView);
+
+new TextView({
+  left: 0, right: 0, top: '50%',
+  alignment: 'center',
+  text: 'Composite 1'
+}).appendTo(composite1);
+
+let composite2 = new Composite({
+  left: '50%', top: 0, bottom: 0, right: 0,
+  background: '#eaeaea'
+}).appendTo(ui.contentView);
+
+new TextView({
+  left: 0, right: 0, top: '50%',
+  alignment: 'center',
+  text: 'Composite 2'
+}).appendTo(composite2);
 
 createTab('Cart', IMAGE_PATH + 'cart.png', IMAGE_PATH + 'cart-filled.png');
 createTab('Pay', IMAGE_PATH + 'card.png', IMAGE_PATH + 'card-filled.png');
@@ -26,4 +48,5 @@ function createTab(title, image, seletedImage) {
     text: 'Content of Tab ' + title
   }).appendTo(tab);
 }
+
 
