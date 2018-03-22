a9e2de8e025ad267320bbbb69aad2b8d3f8aebdc
bettydraws <31779007+bettydraws@users.noreply.github.com>
2018-03-21 23:00:43 -0400

    Changed wording
    Now says to find out whats cool and in the end it says your the cool one

--- timer.js
+++ timer.js
@@ -2,12 +2,12 @@ const {Button, ui} = require('tabris');
 
 new Button({
   centerX: 0, centerY: 0,
-  text: 'Press me!'
+  text: 'Click Here To See Whats Cool'
 }).on('select', ({target}) => {
-  target.text = 'Please wait...';
+  target.text = 'Loading...';
   setTimeout(sayThanks, 2000, target);
 }).appendTo(ui.contentView);
 
 function sayThanks(widget) {
-  widget.text = 'Thank you!';
+  widget.text = 'Its You!';
 }
